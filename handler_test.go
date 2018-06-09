2993e861616327837d4861e2600df2c2f06c5090
sbstjn <mail@sbstjn.com>
2018-06-09 18:09:24 +0200

    Update tests

--- handler_test.go
+++ handler_test.go
@@ -8,33 +8,33 @@ import (
 )
 
 func TestInvalidHandlerFunction(t *testing.T) {
-	assert.Error(t, Handler{true}.validate())
-	assert.Error(t, Handler{412}.validate())
-	assert.Error(t, Handler{""}.validate())
+	assert.Error(t, handler{true}.validate())
+	assert.Error(t, handler{412}.validate())
+	assert.Error(t, handler{""}.validate())
 
-	assert.Error(t, Handler{func() {}}.validate())
-	assert.Error(t, Handler{func() interface{} { return nil }}.validate())
-	assert.Error(t, Handler{func(args struct{}) {}}.validate())
-	assert.Error(t, Handler{func(args struct{}) interface{} { return nil }}.validate())
-	assert.Error(t, Handler{func(args struct{}) (interface{}, interface{}) { return nil, nil }}.validate())
+	assert.Error(t, handler{func() {}}.validate())
+	assert.Error(t, handler{func() interface{} { return nil }}.validate())
+	assert.Error(t, handler{func(args struct{}) {}}.validate())
+	assert.Error(t, handler{func(args struct{}) interface{} { return nil }}.validate())
+	assert.Error(t, handler{func(args struct{}) (interface{}, interface{}) { return nil, nil }}.validate())
 
-	assert.Error(t, Handler{func(args string) (interface{}, error) { return nil, nil }}.validate())
+	assert.Error(t, handler{func(args string) (interface{}, error) { return nil, nil }}.validate())
 
-	assert.Error(t, Handler{func(args struct{}, param struct{}) (interface{}, error) { return nil, nil }}.validate())
+	assert.Error(t, handler{func(args struct{}, param struct{}) (interface{}, error) { return nil, nil }}.validate())
 
-	assert.Error(t, Handler{func(args struct{}) (interface{}, error, error) { return nil, nil, nil }}.validate())
+	assert.Error(t, handler{func(args struct{}) (interface{}, error, error) { return nil, nil, nil }}.validate())
 }
 
 func TestValidHandlerFunction(t *testing.T) {
-	assert.Nil(t, Handler{func(args struct{}) (interface{}, error) { return nil, nil }}.validate())
-	assert.Nil(t, Handler{func(args struct{}) error { return nil }}.validate())
-	assert.Nil(t, Handler{func() (interface{}, error) { return nil, nil }}.validate())
-	assert.Nil(t, Handler{func() error { return nil }}.validate())
+	assert.Nil(t, handler{func(args struct{}) (interface{}, error) { return nil, nil }}.validate())
+	assert.Nil(t, handler{func(args struct{}) error { return nil }}.validate())
+	assert.Nil(t, handler{func() (interface{}, error) { return nil, nil }}.validate())
+	assert.Nil(t, handler{func() error { return nil }}.validate())
 }
 
 func TestArgumentFailsOnInvalidJSON(t *testing.T) {
 	message := json.RawMessage("{\"\"name\": \"example\"}")
-	example := Handler{func(args struct {
+	example := handler{func(args struct {
 		Name string `json:"name"`
 	}) error {
 		return nil
@@ -48,7 +48,7 @@ func TestArgumentFailsOnInvalidJSON(t *testing.T) {
 
 func TestArgumentWorkForValidJSON(t *testing.T) {
 	message := json.RawMessage("{\"name\": \"example\"}")
-	example := Handler{func(args struct {
+	example := handler{func(args struct {
 		Name string `json:"name"`
 	}) error {
 		return nil
@@ -62,7 +62,7 @@ func TestArgumentWorkForValidJSON(t *testing.T) {
 
 func TestArgumentWorkWithoutHandlerArguments(t *testing.T) {
 	message := json.RawMessage("{\"name\": \"example\"}")
-	example := Handler{func() error {
+	example := handler{func() error {
 		return nil
 	}}
 

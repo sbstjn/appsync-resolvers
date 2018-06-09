9f7243be1429907bbf4a6cbcca839aae417531f3
sbstjn <mail@sbstjn.com>
2018-06-09 16:25:02 +0200

    Add tests for route return values

--- router_test.go
+++ router_test.go
@@ -2,6 +2,7 @@ package router_test
 
 import (
 	"encoding/json"
+	"errors"
 	"os"
 	"testing"
 
@@ -13,7 +14,33 @@ var (
 	r = router.New()
 )
 
+func createHandleResponse(route string) (interface{}, error) {
+	return r.Handle(router.Request{
+		Field:     route,
+		Arguments: json.RawMessage("{}"),
+	})
+}
+
 func TestMain(m *testing.M) {
+	r.Add("routeWithArgumentsAndSingleReturnThatReturnsError", func(args struct{ Foo bool }) error {
+		return errors.New("Example Error")
+	})
+	r.Add("routeWithArgumentsAndSingleReturnThatReturnsNil", func(args struct{ Foo bool }) error {
+		return nil
+	})
+	r.Add("routeWithArgumentsAndTwoReturnsThatReturnsError", func(args struct{ Foo bool }) (interface{}, error) {
+		return nil, errors.New("Example Error")
+	})
+	r.Add("routeWithArgumentsAndTwoReturnsThatReturnsData", func(args struct{ Foo bool }) (interface{}, error) {
+		type data struct {
+			Foo bool
+		}
+		return data{true}, nil
+	})
+	r.Add("routeWithArgumentsAndTwoReturnsThatReturnsNil", func(args struct{ Foo bool }) (interface{}, error) {
+		return nil, nil
+	})
+
 	r.Add("validWithoutParameter", func() error { return nil })
 	r.Add("validWithOneParameter", func(args struct {
 		Name string `json:"name"`
@@ -27,6 +54,31 @@ func TestMain(m *testing.M) {
 func TestRouteValidationOnAdd(t *testing.T) {
 	assert.Nil(t, r.Add("validWithoutParameter-again", func() error { return nil }))
 	assert.Error(t, r.Add("invalidHandler", func(args bool) error { return nil }))
+}
+
+func TestRegisteredRoutes(t *testing.T) {
+	var data interface{}
+	var err error
+
+	data, err = createHandleResponse("routeWithArgumentsAndSingleReturnThatReturnsError")
+	assert.Nil(t, data)
+	assert.Error(t, err)
+
+	data, err = createHandleResponse("routeWithArgumentsAndSingleReturnThatReturnsNil")
+	assert.Nil(t, data)
+	assert.Nil(t, err)
+
+	data, err = createHandleResponse("routeWithArgumentsAndTwoReturnsThatReturnsError")
+	assert.Nil(t, data)
+	assert.Error(t, err)
+
+	data, err = createHandleResponse("routeWithArgumentsAndTwoReturnsThatReturnsData")
+	assert.NotNil(t, data)
+	assert.Nil(t, err)
+
+	data, err = createHandleResponse("routeWithArgumentsAndTwoReturnsThatReturnsNil")
+	assert.Nil(t, data)
+	assert.Nil(t, err)
 }
 
 func TestRouteMiss(t *testing.T) {
